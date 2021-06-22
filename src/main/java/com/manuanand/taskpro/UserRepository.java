c6b4b358c867847cc1a7d009f4e8674273aa0df2
Manu Anand <mananan@microsoft.com>
2021-06-22 11:06:38 +0530

    TaskPro: First commit!

---
+++ src/main/java/com/manuanand/taskpro/UserRepository.java
@@ -0,0 +1,12 @@
+package com.manuanand.taskpro;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.manuanand.taskpro.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
